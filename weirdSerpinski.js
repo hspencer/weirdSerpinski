f3d37d59e9435c5fa473b40220f5a0df2e97a1c4
Herbert Spencer <hspencer@ead.cl>
2017-12-14 23:48:47 -0300

    fix

--- weirdSerpinski.js
+++ weirdSerpinski.js
@@ -9,70 +9,66 @@ var num, numO1, num02, prop;
 var maxPoints = 2000;
 var Points = [];
 var inputBox;
-var inputBlock;  // declare the variable that tracks the state of the input box
+var inputBlock;  // declare the boolean that tracks the state of the input box
 
 function setup() {
-  var myCanvas = createCanvas(screen.width, screen.height);
-  myCanvas.parent('sketch');
-  numO1 = document.getElementById('num-output-1');
-  numO2 = document.getElementById('num-output-2');
-  propO = document.getElementById('prop-output');
-  inputBox = document.getElementById('toggle');
-
-  updateInput();
-  inputBlock = false;
+	var myCanvas = createCanvas(screen.width, screen.height);
+	myCanvas.parent('sketch');
+	numO1 = document.getElementById('num-output-1');
+	numO2 = document.getElementById('num-output-2');
+	propO = document.getElementById('prop-output');
+	inputBox = document.getElementById('toggle');
+	cursor(CROSS);
+	updateInput();
+	inputBlock = false;
+	background(255);
 }
 
 function draw() {
-  background(255);
-  if(Points.length < num){
-    for (var i = 0; i < Points.length; i++) {
-      Points[i].draw();
-    }
-  }else if(/*Points.length <= maxPoints && */ Points.length >= num){
-    for (var i = num; i < Points.length; i++) {
-      Points[i].draw();
-    }
-    var randomVertex = Math.floor(random(0, num));
-    Points[randomVertex].draw();
-    var lastX = Points[Points.length-1].x;
-    var lastY = Points[Points.length-1].y;
-    var newX = (lastX + Points[randomVertex].x*(num-2)) / (num-1);
-    var newY = (lastY + Points[randomVertex].y*(num-2)) / (num-1);
-    p = new Point(newX, newY);
-    Points.push(p);
-  }
+	if(Points.length >= num){
+		var randomVertex = Math.floor(random(0, num));
+		Points[randomVertex].draw();
+		var lastX = Points[Points.length-1].x;
+		var lastY = Points[Points.length-1].y;
+		var newX = (lastX + Points[randomVertex].x*(num-2)) / (num-1);
+		var newY = (lastY + Points[randomVertex].y*(num-2)) / (num-1);
+		p = new Point(newX, newY);
+		Points.push(p);
+		p.draw();
+	}
 }
 
 function Point(xpos, ypos) {
-  this.x = xpos;
-  this.y = ypos;
-  this.d = 1.5;                 // diameter
-  this.c = color(0,100);        // color
-  this.draw = function() {
-    stroke(this.c);
-    strokeWeight(this.d);
-    point(this.x, this.y);
-  }
+	this.x = xpos;
+	this.y = ypos;
+	this.d = 2;
+	this.c = color(0, 200);
+	this.draw = function() {
+		stroke(this.c);
+		strokeWeight(this.d);
+		point(this.x, this.y);
+	}
 }
 
 function mouseClicked() {
-  if (Points.length <= num && !inputBlock) {
-    var p = new Point(mouseX, mouseY);
-    p.d = 5;
-    p.c = color(200, 10, 10);
-    Points.push(p);
-  }
+	if (Points.length <= num && !inputBlock) {
+		var p = new Point(mouseX, mouseY);
+		p.d = 5;
+		p.c = color(200, 10, 10);
+		Points.push(p);
+		p.draw();
+	}
 }
 
 function updateInput(){
-  inputBlock = true;
-  Points = [];
-  num = document.getElementById('n').value;
-  numO1.innerHTML = num;
-  numO2.innerHTML = num;
-  prop = ""+(num-2)+"/"+(num-1);
-  propO.innerHTML = prop;
+	background(255);
+	inputBlock = true;
+	Points = [];
+	num = document.getElementById('n').value;
+	numO1.innerHTML = num;
+	numO2.innerHTML = num;
+	prop = ""+(num-2)+"/"+(num-1);
+	propO.innerHTML = prop;
 }
 
 function block(){inputBlock = true;}
